cdcbbce4b76e6d6fa6d777fc9752df1190cc419f
agatunov <agatunov@gmail.com>
2023-04-17 11:43:56 +0700

    compete validateButton_click

--- GatunovSession/Form1.cs
+++ GatunovSession/Form1.cs
@@ -19,7 +19,23 @@ namespace GatunovSession
 
         private void validateButton_Click(object sender, EventArgs e)
         {
+            var errors = new StringBuilder();
+            var validator = new Validator();
 
+            if (!validator.ValidateTextByCharsCount(3, 25, firstnameTextBox.Text))
+            {
+                errors.AppendLine("У поля имя минимальное количество символом - 3, максимальное - 25");
+            }
+            if (!validator.ValidateTextByCharsCount(2, 30, surnameTextBox.Text))
+            {
+                errors.AppendLine("У поля имя минимальное количество символом - 2, максимальное - 25");
+            }
+            if (!validator.ValidateEmail(emailTextBox.Text))
+            {
+                errors.AppendLine("У поля электронная почта маска вида X@X.X, где X - любой количество символов");
+            }
+
+            MessageBox.Show(errors.Length > 0 ? errors.ToString() : "Данные верны");
         }
     }
 }
